0b0750674fd24f196bd4def0ffe1910e198c9f5f
Jerome MORISSARD <morissardj@gmail.com>
2024-10-31 08:30:20 +0100

    Create spacy_test.py

---
+++ Tools/spacy_test.py
@@ -0,0 +1,11 @@
+
+import sys
+import os
+
+import pandas as pd
+
+# from detect_ai_content.ml_logic.for_texts.using_ml_features.using_ml_features import enrich
+
+import spacy
+print(spacy.prefer_gpu())
+print(spacy.require_gpu())
