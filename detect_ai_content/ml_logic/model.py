975cdb0988d5f47353e729bdd44c561906bbade5
Abhinav Banerjee <371818.aban@gmail.com>
2024-11-03 15:45:42 +0000

    code changes to the cnn ml logic

--- detect_ai_content/ml_logic/model.py
+++ detect_ai_content/ml_logic/model.py
@@ -8,20 +8,18 @@ ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 model_path = os.path.join(ROOT_PATH, 'models', 'cnn_model.h5')
 model = load_model(model_path)
 
-def fake_real():
+def pre_process(image_path):
 
-    def pre_process(image_path):
+    img = image.load_img(image_path, target_size=(180, 180))
+    img_array = image.img_to_array(img)
+    img_array = img_array/255
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
 
-        img = image.load_img(image_path, target_size=(180, 180))
-        img_array = image.img_to_array(img)
-        img_array = img_array/255
-        img_array = np.expand_dims(img_array, axis=0)
-        return img_array
+def predict (image_path):
 
-    def predict (image_path):
-
-        to_predict_img_array = pre_process(image_path)
-        prediction = model.predict(to_predict_img_array)
-        if prediction < 0.5:
-            return 'FAKE'
-        return 'REAL'
+    to_predict_img_array = pre_process(image_path)
+    prediction = model.predict(to_predict_img_array)
+    if prediction < 0.5:
+        return 'FAKE'
+    return 'REAL'
