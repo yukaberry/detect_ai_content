42394209eb629868a9346a2188bb001e39d13e82
Jerome MORISSARD <morissardj@gmail.com>
2024-11-03 20:29:36 +0100

    improve Text analyse UX

--- detect_ai_content/streamlit/pages/1_text_genAi_detection.py
+++ detect_ai_content/streamlit/pages/1_text_genAi_detection.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import requests
+import time
+
+st.subheader("Text to analyze")
 
 txt = st.text_area(
-    "Text to analyze",
+    " ",
     "It was the best of times, it was the worst of times, it was the age of "
     "wisdom, it was the age of foolishness, it was the epoch of belief, it "
     "was the epoch of incredulity, it was the season of Light, it was the "
@@ -11,16 +14,15 @@ txt = st.text_area(
 )
 
 st.write(f"You wrote {len(txt)} characters.")
-
-st.subheader("Prediction - get text")
-st.write(f"{txt}")
-
-st.subheader("Prediction - predict")
-headers = {
-    'accept': 'application/json',
-}
-params = {
-    "text":txt
-}
-response = requests.get('https://detect-ai-content-j-mvp-667980218208.europe-west1.run.app/predict', headers=headers, params=params)
-st.write(f"{response.json()}")
+if st.button('Evaluate this text'):
+    st.subheader("Prediction - predict")
+    with st.spinner('Wait for it...'):
+        time.sleep(2)
+        headers = {
+            'accept': 'application/json',
+        }
+        params = {
+            "text":txt
+        }
+        response = requests.get('https://detect-ai-content-j-mvp-667980218208.europe-west1.run.app/predict', headers=headers, params=params)
+        st.success(f"{response.json()}")
