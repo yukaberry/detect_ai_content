12d6d996836a926ae52e5beb9bd2e1e5193b62d7
Jerome MORISSARD <morissardj@gmail.com>
2024-11-04 16:21:57 +0100

    update streamlit project

--- detect_ai_content/streamlit/app.py
+++ detect_ai_content/streamlit/app.py
@@ -1,16 +1,10 @@
 import streamlit as st
-import sklearn
 
 '''
 # Detect AI Content front
 '''
 
-import datetime
 import streamlit as st
-import pickle
-
-from detect_ai_content.ml_logic.preprocess import preprocess_text
-
 
 st.subheader("🔑 Key concepts")
 st.write(f"Explain the basis of the projet ")
