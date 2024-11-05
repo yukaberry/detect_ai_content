8efaf1add4cd30b37ff08c7892cce7abb8208c78
Jerome MORISSARD <morissardj@gmail.com>
2024-11-05 09:00:58 +0100

    Updates tests
    Test can be run using the command :
    
    python tests/test_image_modeling_TrueNetImageUsinCustomCNN.py

--- tests/test_image_modeling_TrueNetImageUsinCustomCNN.py
+++ tests/test_image_modeling_TrueNetImageUsinCustomCNN.py
@@ -11,11 +11,11 @@ class TestImageTrueNetImageUsinCustomCNN(unittest.TestCase):
     def test_predict_using_model(self):
         import detect_ai_content
         module_dir_path = os.path.dirname(detect_ai_content.__file__)
-        image_path = os.path.join(f'{module_dir_path}/..', 'tests','data', 'test-image.jpg')
+        image_path = os.path.join(f'{module_dir_path}/..', 'tests','data', 'real_0192.jpg')
 
         model = TrueNetImageUsinCustomCNN()
         y_pred = model.predict(image_path)
-        print(y_pred)
+        self.assertEqual(y_pred == 0 or y_pred == 1, True, msg="Image from the kaggle-cifake-real-and-ai-generated-synthetic dataset")
 
 if __name__ == '__main__':
     unittest.main()
