3b8c2b6386dfb084d0ebe7042b91638bb119ff1d
Jerome MORISSARD <morissardj@gmail.com>
2024-11-06 14:35:22 +0100

    Update params.py

--- detect_ai_content/params.py
+++ detect_ai_content/params.py
@@ -1,7 +1,3 @@
 
 MODEL_TARGET="mlflow"
 MLFLOW_TRACKING_URI="https://mlflow.lewagon.ai"
-
-# Vectorizer model
-MLFLOW_VECTORIZER_MODEL_NAME="TrueNet_Tfid_NaiveBayes_detect_ai_content"
-MLFLOW_VECTORIZER_EXPERIMENT="TrueNet_Tfid_NaiveBayes_experiment_leverdewagon"
