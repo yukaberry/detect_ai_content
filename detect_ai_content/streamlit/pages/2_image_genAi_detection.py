f436715f5497390874b3fb40419c63f346f08b74
Yuka Kudo <yukaausberry@gmail.com>
2024-11-07 20:01:41 +0100

    deployed url is updated on streamlit

--- detect_ai_content/streamlit/pages/2_image_genAi_detection.py
+++ detect_ai_content/streamlit/pages/2_image_genAi_detection.py
@@ -62,6 +62,6 @@ if path_in is not None:
             }
             # local http://0.0.0.0:8000/image_predict
             #'https://detect-ai-content-j-mvp-667980218208.europe-west1.run.app/image_predict', headers=headers, files=files)
-            response = requests.post("https://detect-ai-content-667980218208.europe-west1.run.app/image_predict",headers=headers, files=files)
+            response = requests.post("https://detect-ai-content2-667980218208.europe-west1.run.app/image_predict",headers=headers, files=files)
             # st.success(f"{response.json()}")
             st.success(response.text)
