f995da54b08b9469a1ee42258079028d56c80d75
Yuka Kudo <yukaausberry@gmail.com>
2024-11-20 14:13:07 +0100

    function return 3 values

--- detect_ai_content/ml_logic/for_images/image_classifier_cnn.py
+++ detect_ai_content/ml_logic/for_images/image_classifier_cnn.py
@@ -21,20 +21,43 @@ class image_classifier_cnn:
         return model
 
     def pre_process(self,image_path):
+
         img = image.load_img(image_path, target_size=(225, 225))
+        #img = img.resize((225, 225))
         img_array = image.img_to_array(img)
         img_array = img_array/255
         img_array = np.expand_dims(img_array, axis=0)
         return img_array
 
     def predict(self, image_path):
-        to_predict_img_array = self.pre_process(image_path)
-        prediction = self.model.predict(to_predict_img_array)
-        pred = float(prediction[0][0])
 
-        # pred
-        rounded_pred = round(pred)
-        return rounded_pred
+        """
+        - preprocess input img data
+        - retun 3 values
+        1. prediction class (1 or 0)
+        2. message (Ai or Human)
+        3. Probability (2 dicimal)
+
+        """
+        to_predict_img_array = self.pre_process(image_path)
+
+        prediction_results = self.model.predict(to_predict_img_array)
+        class_prediction = float(prediction_results[0][0])
+        print(prediction_results)
+
+        predict_prob_confidence = round(abs(class_prediction - 0.5)/0.5, 2)
+
+        # # predict class
+        predicted_class = int(class_prediction > 0.5)
+
+        # 0 likely representing 'FAKE' and 1 representing 'REAL'
+        if predicted_class == 0:
+            prediction_message = "Predicted as AI"
+        elif predicted_class == 1 :
+            prediction_message = "Predicted as Human"
+
+        return predicted_class, prediction_message, predict_prob_confidence
+
 
         # if round(float(prediction[0][0]) * 100, 2) > 0.5:
             #return "The image is probably real :D"
