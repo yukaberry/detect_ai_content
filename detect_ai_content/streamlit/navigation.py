ce7d79b67a1b525cf2885108f64bd38ca1acb530
Jerome MORISSARD <morissardj@gmail.com>
2024-11-21 21:53:39 +0100

    FIX things
    - Update streamit version to =1.40.1, to be able to use the new st.pills component
    - Update CSS style to have an homogenous UI
    - Finish the Single Image prediction (results were not presented)

--- detect_ai_content/streamlit/navigation.py
+++ detect_ai_content/streamlit/navigation.py
@@ -6,12 +6,12 @@ pages = {
         st.Page("./app_v0.py", title="Home"),
     ],
     "📖 AI detection for texts": [
-        st.Page("./pages/1_Text AI_Detector.py", title="One model"),
-        st.Page("./pages/2_Multi-Model_Text_AI_Detector.py", title="Multiple models"),
+        st.Page("./pages/1_Text AI_Detector.py", title="Predict using the best model"),
+        st.Page("./pages/2_Multi-Model_Text_AI_Detector.py", title="Challenge our models"),
     ],
     "🌌 AI detection for images": [
-        st.Page("./pages/3_Image_AI_Detector.py", title="One model"),
-        st.Page("./pages/page_multi_imges.py", title="Multiple models"),
+        st.Page("./pages/3_Image_AI_Detector.py", title="Predict using the best model"),
+        st.Page("./pages/page_multi_imges.py", title="Challenge our models"),
     ],
     "ℹ️ More pages ": [
         st.Page("./pages/page_approach.py", title="Approach"),
